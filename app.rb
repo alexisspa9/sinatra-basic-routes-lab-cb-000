bc91dbf1a472b2fea5eb0c4dc1cbd5a4ce209c5b
Alexander Spanos <alexisspa9@gmail.com>
2018-04-01 19:37:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Alexander"
+  end
+  get '/hometown' do
+    "My hometown is Paros"
+  end
+  get '/favourite-song' do
+    "My favourite song in Bullishit"
+  end
 end
